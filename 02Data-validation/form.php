65291b4c3e29f6d3c2d25eb2b25c69e5f38788b4
Gabriel Mejia <gabopos@gmail.com>
2024-05-29 23:03:33 -0500

    optimize Data Validation with JS

--- 02Data-validation/form.php
+++ 02Data-validation/form.php
@@ -12,7 +12,7 @@
     <input type="text" name="name_txt">
     <br><br>
     <label>Enter your password:</label>
-    <input type="text" name="pwd_txt">
+    <input type="password" name="pwd_txt">
     <br><br>
     <input type="radio" name="gender_rdo" value="M">
     <label>Masculino&nbsp;</label>
@@ -22,19 +22,53 @@
     <input type="hidden" name="send_hdn" value="get">
     <input id="send-get" type="button" name="send_btn" value="GET send" >
 <script>
-  const getDataValidation = () => {
-    let check = true,
-      d = document;
+  // const getDataValidation = () => {
+  //   let check = true,
+  //     d = document;
 
-    if(!d.data_validate_get_frm.name_txt.value){
-      alert("The Name field is required!");
-      d.data_validate_get_frm.name_txt.focus();
-      check = false;
-    }        
+  //   if(!d.data_validate_get_frm.name_txt.value){
+  //     alert("The Name field is required!");
+  //     d.data_validate_get_frm.name_txt.focus();
+  //     check = false;
+  //   } else if (!d.data_validate_get_frm.pwd_txt.value) {
+  //     alert("The Password field is required!");
+  //     d.data_validate_get_frm.pwd_txt.focus();
+  //     check = false;
+  //   } else if (!d.data_validate_get_frm.gender_rdo[0].checked && !d.data_validate_get_frm.gender_rdo[1].checked) {
+  //     alert("Gender field is required!");
+  //     d.data_validate_get_frm.gender_rdo[0].focus();
+  //     check = false;
+  //   }
+  // }
+  // window.onload = function() {
+  //     document.getElementById("send-get").onclick = getDataValidation;
+  //   }
+  const getDataValidation = () => {
+  const d = document;
+  const nameValue = d.data_validate_get_frm.name_txt.value;
+  const pwdValue = d.data_validate_get_frm.pwd_txt.value;
+  const genderChecked = d.data_validate_get_frm.gender_rdo[0].checked || d.data_validate_get_frm.gender_rdo[1].checked;
+
+  if (!nameValue) {
+    alert("The Name field is required!");
+    d.data_validate_get_frm.name_txt.focus();
+    return false;
+  } else if (!pwdValue) {
+    alert("The Password field is required!");
+    d.data_validate_get_frm.pwd_txt.focus();
+    return false;
+  } else if (!genderChecked) {
+    alert("Gender field is required!");
+    d.data_validate_get_frm.gender_rdo[0].focus();
+    return false;
   }
-  window.onload = function() {
-      document.getElementById("send-get").onclick = getDataValidation;
-    }
+  return true;
+};
+
+window.onload = () => {
+  document.getElementById("send-get").onclick = getDataValidation;
+};
+
 
 </script>
 </body>
