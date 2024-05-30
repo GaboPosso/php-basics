8e8d34d3feadbab1490ad2ed6d7d7100f3476a54
Gabriel Mejia <gabopos@gmail.com>
2024-05-29 23:44:56 -0500

    End HTML file for Data Validation with JS

--- 02Data-validation/form.php
+++ 02Data-validation/form.php
@@ -15,58 +15,84 @@
     <input type="password" name="pwd_txt">
     <br><br>
     <input type="radio" name="gender_rdo" value="M">
-    <label>Masculino&nbsp;</label>
+    <label>Male&nbsp;</label>
     <input type="radio" name="gender_rdo" value="F">
-    <label>Femenino&nbsp;</label>
+    <label>Female&nbsp;</label>
     <br><br>
     <input type="hidden" name="send_hdn" value="get">
     <input id="send-get" type="button" name="send_btn" value="GET send" >
-<script>
-  // const getDataValidation = () => {
-  //   let check = true,
-  //     d = document;
+  </form>
 
-  //   if(!d.data_validate_get_frm.name_txt.value){
-  //     alert("The Name field is required!");
-  //     d.data_validate_get_frm.name_txt.focus();
-  //     check = false;
-  //   } else if (!d.data_validate_get_frm.pwd_txt.value) {
-  //     alert("The Password field is required!");
-  //     d.data_validate_get_frm.pwd_txt.focus();
-  //     check = false;
-  //   } else if (!d.data_validate_get_frm.gender_rdo[0].checked && !d.data_validate_get_frm.gender_rdo[1].checked) {
-  //     alert("Gender field is required!");
-  //     d.data_validate_get_frm.gender_rdo[0].focus();
-  //     check = false;
-  //   }
-  // }
-  // window.onload = function() {
-  //     document.getElementById("send-get").onclick = getDataValidation;
-  //   }
+  <hgroup><h1>Data Form POST</h1></hgroup>
+  <form name="data_validate_post_frm" action="data-validate.php" method="post" enctype="application/x-www-form-urlencoded">
+    <label>Enter your name:</label>
+    <input type="text" name="name_txt">
+    <br><br>
+    <label>Enter your password:</label>
+    <input type="password" name="pwd_txt">
+    <br><br>
+    <input type="radio" name="gender_rdo" value="M">
+    <label>Male&nbsp;</label>
+    <input type="radio" name="gender_rdo" value="F">
+    <label>Female&nbsp;</label>
+    <br><br>
+    <input type="hidden" name="send_hdn" value="post">
+    <input id="send-post" type="button" name="send_btn" value="POST send" >
+  </form>
+  
+<script>
   const getDataValidation = () => {
   const d = document;
-  const nameValue = d.data_validate_get_frm.name_txt.value;
-  const pwdValue = d.data_validate_get_frm.pwd_txt.value;
-  const genderChecked = d.data_validate_get_frm.gender_rdo[0].checked || d.data_validate_get_frm.gender_rdo[1].checked;
+  let check = true;
+  let nameValue = d.data_validate_get_frm.name_txt.value;
+  let pwdValue = d.data_validate_get_frm.pwd_txt.value;
+  let genderChecked = d.data_validate_get_frm.gender_rdo[0].checked || d.data_validate_get_frm.gender_rdo[1].checked;
 
   if (!nameValue) {
     alert("The Name field is required!");
     d.data_validate_get_frm.name_txt.focus();
-    return false;
+    check = false;
   } else if (!pwdValue) {
     alert("The Password field is required!");
     d.data_validate_get_frm.pwd_txt.focus();
-    return false;
+    check = false;
   } else if (!genderChecked) {
     alert("Gender field is required!");
     d.data_validate_get_frm.gender_rdo[0].focus();
-    return false;
+    check = false;
+  }  if(check) {
+    d.data_validate_get_frm.submit();
   }
-  return true;
 };
+
+const postDataValidation = () => {
+  const d = document;
+  let nameValue = d.data_validate_post_frm.name_txt.value;
+  let pwdValue = d.data_validate_post_frm.pwd_txt.value;
+  let genderChecked = d.data_validate_post_frm.gender_rdo[0].checked || d.data_validate_post_frm.gender_rdo[1].checked;
+  let check = true;
+  if (!nameValue) {
+    alert("The Name field is required!");
+    d.data_validate_post_frm.name_txt.focus();
+    check = false;
+  } else if (!pwdValue) {
+    alert("The Password field is required!");
+    d.data_validate_post_frm.pwd_txt.focus();
+    check = false;
+  } else if (!genderChecked) {
+    alert("Gender field is required!");
+    d.data_validate_post_frm.gender_rdo[0].focus();
+    check = false;
+  }
+  if(check) {
+    d.data_validate_post_frm.submit();
+  }
+};
+
 
 window.onload = () => {
   document.getElementById("send-get").onclick = getDataValidation;
+  document.getElementById("send-post").onclick =postDataValidation;
 };
 
 
