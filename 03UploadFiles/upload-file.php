1013885b8362c552b1df87ee4f112edae62d9f4c
Gabriel Mejia <gabopos@gmail.com>
2024-05-31 23:59:24 -0500

    End Upload file to server exercise

--- 03UploadFiles/upload-file.php
+++ 03UploadFiles/upload-file.php
@@ -3,8 +3,9 @@
     echo "Property: $key --- Value: $value<br/>";
   }
 
-  $file = $_FILES["file_fls]["tmp_name"];
-  $location = "files/"._FILES["file_fls]["name"];
+  $file = $_FILES["file_fls"]["tmp_name"];
+  $location = "files/".$_FILES["file_fls"]["name"];
 
   move_uploaded_file($file, $location);
+  echo "Uploaded successfully"
 ?>
