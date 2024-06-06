ec6ecc2e3a17e0d94c8992a2042eeb2cce0623a1
Gabriel Mejia <gabopos@gmail.com>
2024-06-05 23:57:56 -0500

    Upload Index for PHP sessions execise

---
+++ 05Sessions/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Sessions</title>
+</head>
+
+<body>
+  <form name="authentication_frm" action="control.php" method="post" enctype="application/x-www-form-urlencoded">
+    User: <input type="text" name="user_txt" id=""> <br><br>
+    Password: <input type="password" name="pwd_txt" id=""> <br><br>
+    <input type="submit" name="enter_btn" value="Enter">
+  </form>
+</body>
+
+</html>
