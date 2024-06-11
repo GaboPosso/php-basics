5351b9cff268af20ff4f58314f78afe5c184e21d
Gabriel Mejia <gabopos@gmail.com>
2024-06-10 20:39:33 -0500

    Upload php mailer

--- 06Mail/form-phpmailer.php
+++ 06Mail/form-phpmailer.php
@@ -19,16 +19,17 @@
 </head>
 
 <body>
-  <form name="mail_frm" action="send-mail.php" method="post" enctype="application/x-www-form-urlencoded">
+  <form name="mail_frm" action="send-mail.php" method="post" enctype="multipart/form-data">
     From: <input type="text" name="from_txt"> <br><br>
     For: <input type="text" name="for_txt"> <br><br>
     Subject: <input type="text" name="subject_txt"> <br><br>
+    Attachment: <input type="file" name="file_fls"> <br><br>
     Message: <br> <textarea name="message_txa"></textarea> <br><br>
     <input type="button" name="send_btn" value="Send"> <br><br>
     <?php
     error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
     if (isset($_GET["response"])) {
-      echo "<span>".$_GET["response"]."</span>";
+      echo "<span>" . $_GET["response"] . "</span>";
     }
     ?>
   </form>
@@ -37,7 +38,7 @@
       let verify = true;
       let expRegEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
-      if (!document.mail_frm.from_txt.value) { 
+      if (!document.mail_frm.from_txt.value) {
         alert("The From field is required!");
         document.mail_frm.from_txt.focus();
         verify = false;
@@ -57,17 +58,21 @@
         alert("The subject field is required!");
         document.mail_frm.subject_txt.focus();
         verify = false;
+      } else if (!document.mail_frm.file_fls.value) {
+        alert("The files upload is required!");
+        document.mail_frm.file_fls.focus();
+        verify = false;
       } else if (!document.mail_frm.message_txa.value) {
         alert("The Message field is required!");
         document.mail_frm.message_txa.focus();
         verify = false;
-      }  
+      }
       if (verify) {
         document.mail_frm.submit();
       }
     }
 
-    window.onload =function() {
+    window.onload = function() {
       document.mail_frm.send_btn.onclick = formValidation;
     }
   </script>
--- 06Mail/send-mail.php
+++ 06Mail/send-mail.php
@@ -1,12 +1,15 @@
-<?php 
- $from = $_POST["from_txt"];
- $for = $_POST["for_txt"];
- $subject = $_POST["subject_txt"];
- $message = $_POST["message_txa"];
- $headers = "MIME-Version: 1.0\r\n";
- $headers .= "Content-Type: text/html; charset=iso-8859-1\r\n";
- $headers.= "From: $from\r\n";
+<?php
+$from = $_POST["from_txt"];
+$for = $_POST["for_txt"];
+$subject = $_POST["subject_txt"];
+$message = $_POST["message_txa"];
+$headers = "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=iso-8859-1\r\n";
+$headers .= "From: $from\r\n";
 
- mail($for, $subject, $message, $headers) 
-
-?>
+if(mail($for, $subject, $message, $headers)){
+  $response = "Message has been sent.";
+} else {
+  $response = "An error has occurred. Data has not been sent.";
+}
+ header("Location: form-mail.php?response=$response");
