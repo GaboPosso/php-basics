7e66dad36fa2780fb730aafd7a0ba221953a3fe6
Gabriel Mejia <gabopos@gmail.com>
2024-06-17 23:35:18 -0500

    Start Hash project

---
+++ web-applications-php/hash/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Gabriel Mejia</title>
+</head>
+<body>
+  <h1>MD5 cracker</h1>
+
+  <p>This application tasked an MD5 hash of four digits pind and check all 10,000 possible four digits PINs to determine the PIN.</p>
+  <form action="get">
+    <input type="text" name="md5" size="50">
+    <input type="submit" value="Crack MD5">
+  </form>
+</body>
+</html>
